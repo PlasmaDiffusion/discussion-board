862a42f80b2ac74de70332479080b615633f4be1
Scott Cooper <18429915+PlasmaDiffusion@users.noreply.github.com>
2022-06-17 15:56:33 -0400

    Placeholder form and Login
    -Form with a message that logs data when you hit submit
    -Empty Landing Page and Login Components

---
+++ client/components/login/loginButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginButton() {
+  return (
+    <>
+      <button type="button"> Login </button>
+    </>
+  );
+}
+
+export default LoginButton;
--- client/components/posts/postForm.jsx
+++ client/components/posts/postForm.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PostForm() {
-  function OnSubmit() {}
+  const [message, setMessage] = useState("");
+
+  //TODO: Get logged in user/email and submit to server
+  function OnSubmit(e) {
+    e.preventDefault();
+    console.log(message);
+  }
 
   return (
-    <>
-      <form>
-        <input type="text"></input>
+    <div style={{ border: "1px solid black", padding: "10px" }}>
+      <h2>Submit a new post </h2>
+      <br />
+
+      <form onSubmit={OnSubmit}>
+        <textarea
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        ></textarea>
+
+        <br />
+        <input type="submit" />
       </form>
-    </>
+    </div>
   );
 }
 
