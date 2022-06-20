1e1bf608e09a58d2cd1ba1aaefe68d50a1c87962
Scott Cooper <18429915+PlasmaDiffusion@users.noreply.github.com>
2022-06-20 18:54:17 -0400

    Post form size/margin/padding adjustments

--- imports/ui/posts/postForm.jsx
+++ imports/ui/posts/postForm.jsx
@@ -19,6 +19,7 @@ function PostForm({ user }) {
     );
   };
 
+  // Send form to back end and reset form's text
   function OnSubmit(e) {
     e.preventDefault();
     createPost();
@@ -31,12 +32,13 @@ function PostForm({ user }) {
   }
 
   return (
-    <div style={{ border: "1px solid black", padding: "10px" }}>
+    <div>
       <h2 className="formTitle">Submit a New Post</h2>
       <br />
 
-      <form onSubmit={OnSubmit}>
+      <form onSubmit={OnSubmit} className="postForm">
         <textarea
+          
           onChange={(e) => {
             setMessage(e.target.value);
           }}
@@ -47,7 +49,7 @@ function PostForm({ user }) {
 
         <div>
           <label style={{ color: color }}>Post Color: </label>
-          <button type="button" className="coloredButton" onClick={selectPostColor} value="#000000" style={{ backgroundColor: "#000000" }} />
+          <button type="button" className="coloredButton" onClick={selectPostColor} value="#000000" style={{ backgroundColor: "#000000"}} />
           <button type="button" className="coloredButton" onClick={selectPostColor} value="#6495ED" style={{ backgroundColor: "#6495ED" }} />
           <button type="button" className="coloredButton"onClick={selectPostColor} value="#8A2BE2" style={{ backgroundColor: "#8A2BE2" }} />
           <button type="button" className="coloredButton" onClick={selectPostColor} value="#A52A2A" style={{ backgroundColor: "#A52A2A" }} />
--- imports/ui/posts/postList.jsx
+++ imports/ui/posts/postList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Post from "./post";
 
 import { useTracker } from "meteor/react-meteor-data";
