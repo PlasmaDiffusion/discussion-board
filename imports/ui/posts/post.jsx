02601b2f92d7ea87a0a7522d8f644243e0e0b47e
Scott Cooper <18429915+PlasmaDiffusion@users.noreply.github.com>
2022-06-21 16:00:03 -0400

    Post messages display line breaks properly

--- imports/ui/posts/post.jsx
+++ imports/ui/posts/post.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //Shows a user's post, email and a color on the feed from provided props
 function Post({ email, message, color }) {
+  
+  // Check for line breaks, then split it in an array to be output properly
+  const messageWithLineBreaks = () => {
+    if (!message) return message;
+    const splitMessage = message.split("\n");
+    if (splitMessage.length <= 1) return message;
+
+    const resultArr = [];
+    splitMessage.forEach((item, i) => {
+      resultArr.push(item);
+      resultArr.push(<br />);
+    });
+
+    return resultArr;
+  };
+
   return (
     <>
       <div className="post" style={{ outline: `2px solid ${color}` }}>
         <h3 style={{ backgroundColor: color, color: "white" }}>{email}</h3>
-        <p>{message}</p>
+        <p>{messageWithLineBreaks()}</p>
       </div>
     </>
   );
 }
 
-export default Post;
+export default React.memo(Post);
